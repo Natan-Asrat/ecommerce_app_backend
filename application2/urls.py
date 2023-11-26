7d2f8f7f0ccc2ea78109abe9814f686956841f17
Dev <nathanyilmaasrat@gmail.com>
2023-11-26 17:44:34 -0500

    first deploy bug12

--- application2/urls.py
+++ application2/urls.py
@@ -8,6 +8,3 @@ urlpatterns = [
     path('app/', include('posts.urls')),
     path('__debug__/', include(debug_toolbar.urls))
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- posts/urls.py
+++ posts/urls.py
@@ -23,5 +23,3 @@ urlpatterns = [
     path('create_recommendations', views.create_recommendations_api),
     path('update_last_seen', views.update_last_seen)
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
