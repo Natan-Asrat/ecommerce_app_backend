d116c368ff7b5dccb6f77f321596fd6d25879073
Dev <nathanyilmaasrat@gmail.com>
2023-11-27 13:25:42 -0500

    migrate data3

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
 echo 'migrating'
-python3.9 manage.py loaddata data.json
+python3.9 manage.py loaddata output.xml
 # python3.9 manage.py makemigrations
 # python3.9 manage.py migrate
