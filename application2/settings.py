104fdab1c80195a46d286f4fe26ad3ee3352df0c
Dev <nathanyilmaasrat@gmail.com>
2023-11-28 14:29:36 -0500

    tryna connect android to backend5

--- application2/settings.py
+++ application2/settings.py
@@ -196,7 +196,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # }
 
 
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',  # Default authentication backend
-    'posts.backends.FirebaseAuthentication',  # Custom Firebase authentication backend
+AUTHENTICATION_BACKENDS = [  # Default authentication backend
+    'posts.backends.FirebaseAuthentication'  # Custom Firebase authentication backend
 ]
