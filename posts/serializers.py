e65640d91f3059d28c6420f8e31bbeb0d9030d4a
Dev <nathanyilmaasrat@gmail.com>
2024-01-06 16:04:38 -0500

    reason of transaction for virtual payment

--- posts/serializers.py
+++ posts/serializers.py
@@ -827,7 +827,11 @@ class BuyPackageSerializer(serializers.Serializer):
                 #adding to the issuedFor coins is done in signals.py
             
         if tip is True:
-            reason = "Tip " + " " + currency + " " + str(price) + " to " + str(issuedForObj.first_name)
+            if virtual is True:
+                reason = "Tip " +  " " + str(coinAmountInt) + " " + currency + " to " + str(issuedForObj.first_name)
+            else:
+                reason = "Tip " +  " " + str(price) + " " + currency + " to " + str(issuedForObj.first_name)
+
             title = "Tip"
         else:
             reason = "Buy " + amount + " Package"
