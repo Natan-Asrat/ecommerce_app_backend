8c4c1e95693a8e5fb98e5c0a196b38d25548882e
Dev <nathanyilmaasrat@gmail.com>
2024-01-14 06:50:04 -0500

    debug

--- posts/urls.py
+++ posts/urls.py
@@ -36,7 +36,7 @@ router.register('admin_pending_transactions', views.AdminPendingTransactions)
 router.register('admin_rejected_transactions', views.AdminRejectedTransactions)
 router.register('admin_verified_transactions', views.AdminVerifiedTransactions)
 router.register('admin_recent_transactions', views.AdminRecentTransactions)
-router.register('devices', FCMDeviceViewSet)
+router.register('devices', views.FCM)
 
 profile_router = routers.NestedDefaultRouter(router, 'profiles', lookup = 'seller')
 profile_router.register('profile_posts', views.ProfilePostsAPI, basename='profile-posts')
